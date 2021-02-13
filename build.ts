58c1bf57386f6d87671c44dd31702b66bc1a710e
Soner Sayakci <s.sayakci@shopware.com>
2021-02-13 21:49:33 +0100

    Initial commit

---
+++ build.ts
@@ -0,0 +1,30 @@
+const productKeys = [
+    'PS', // PhpStorm
+    'GO', // GoLand
+];
+
+async function main() {
+    const ghConfig = {
+        'fail-fast': false,
+        matrix: {
+            include: [] as any
+        }
+    };
+
+    for (let productKey of productKeys) {
+        let json = await (await fetch(`https://data.services.jetbrains.com/products/releases?code=${productKey}&latest=true&type=release&build=&_=${new Date().getTime()}`)).json() as any;
+
+        for (let release of json[productKey]) {
+            ghConfig.matrix.include.push({
+                name: `${productKey} ${release.version}`,
+                runs: {
+                    build: ` cd projector-docker; ./projector-docker/build-container.sh ghcr.io/shyim/jetbrains-ide/${productKey}:${release.version} ${release.downloads.linux.link}; docker push ghcr.io/shyim/jetbrains-ide/${productKey}:${release.version}`
+                }
+            });
+        }
+    }
+
+    await Deno.stdout.write(new TextEncoder().encode(JSON.stringify(ghConfig)));
+}
+
+main();
